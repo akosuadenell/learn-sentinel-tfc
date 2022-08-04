546eb2ff7c594575ad601a030ae6f2ab8da2b40d
akosuadenell <29132813+akosuadenell@users.noreply.github.com>
2022-08-04 16:12:21 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -34,5 +34,11 @@ resource "aws_instance" "ubuntu2" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = var.instance_type
   
+  
+    #tags = {
+    #Name = var.instance_name
+    #Purpose = "Demo testing"
+    #State = "Texas"
+  }
 
 }
